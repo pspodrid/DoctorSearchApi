37ee03d6c2434e1fce748438b98d0c5dd9de9376
Student <example@example.com>
2019-11-22 11:48:57 -0800

    try to troubleshoot API error (too many requests

--- src/back.js
+++ src/back.js
@@ -2,7 +2,7 @@ export class Hospital {
 
   async apiDocName(docName) {
     try {
-      let response = fetch(`https://api.betterdoctor.com/2016-03-01/doctors?name=${docName}&location=37.773%2C-122.413%2C100&user_location=37.773%2C-122.413&skip=0&limit=10&user_key=${process.env.API_KEY}`)
+      let response = fetch(`https://api.betterdoctor.com/2016-03-01/doctors?name=${docName}&location=45.5051%2C-122.6750%2C100&user_location=45.5051%2C-122.6750&skip=0&limit=10&user_key=${process.env.API_KEY}`)
       if(response.ok) {
         let jsonifiedResponse = await response.json();
         return jsonifiedResponse;
@@ -11,7 +11,7 @@ export class Hospital {
         throw new Error('my error');
       }
     } catch(error) {
-      let response = await fetch (`https://api.betterdoctor.com/2016-03-01/doctors?name=${docName}&location=37.773%2C-122.413%2C100&user_location=37.773%2C-122.413&skip=0&limit=10&user_key=${process.env.API_KEY}`)
+      let response = await fetch (`https://api.betterdoctor.com/2016-03-01/doctors?name=${docName}&location=45.5051%2C-122.6750%2C100&user_location=45.5051%2C-122.6750&skip=0&limit=10&user_key=${process.env.API_KEY}`)
       if (!response.ok) {
         let jsonifiedResponse = await response.json();
         return jsonifiedResponse;
