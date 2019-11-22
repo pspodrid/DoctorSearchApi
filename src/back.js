cb0d6b0f97a77384c1ccaa9281dcf9d09b41e859
Student <example@example.com>
2019-11-22 10:13:32 -0800

    create logic to have API talk to me

--- src/back.js
+++ src/back.js
@@ -1,21 +1,31 @@
+// export class Hospital {
+//
+//   async apiDocName(docName) {
+//     try {
+//       let response = fetch(`https://api.betterdoctor.com/2016-03-01/doctors?name=${docName}&location=37.773%2C-122.413%2C100&user_location=37.773%2C-122.413&skip=0&limit=10&user_key=${process.env.API_KEY}`)
+//       if(response.ok) {
+//         let jsonifiedResponse = await response.json();
+//         return jsonifiedResponse;
+//
+//       } else {
+//         throw new Error('my error');
+//       }
+//     } catch(error) {
+//       let response = await fetch (`https://api.betterdoctor.com/2016-03-01/doctors?name=${docName}&location=37.773%2C-122.413%2C100&user_location=37.773%2C-122.413&skip=0&limit=10&user_key=${process.env.API_KEY}`)
+//       if (!response.ok) {
+//         let jsonifiedResponse = await response.json();
+//         return jsonifiedResponse;
+//       }
+//     }
+//   }
+// }
+
 export class Hospital {
-
   async apiDocName(docName) {
-    try {
-      let response = fetch(`https://api.betterdoctor.com/2016-03-01/doctors?name=${docName}&location=37.773%2C-122.413%2C100&user_location=37.773%2C-122.413&skip=0&limit=10&user_key=${process.env.API_KEY}`)
-      if(response.ok) {
-        let jsonifiedResponse = await response.json();
-        return jsonifiedResponse;
-
-      } else {
-        throw new Error('my error');
-      }
-    } catch(error) {
-      let response = await fetch (`https://api.betterdoctor.com/2016-03-01/doctors?name=${docName}&location=37.773%2C-122.413%2C100&user_location=37.773%2C-122.413&skip=0&limit=10&user_key=${process.env.API_KEY}`)
-      if (!response.ok) {
-        let jsonifiedResponse = await response.json();
-        return jsonifiedResponse;
-      }
-    }
+    let response =  await fetch(`https://api.betterdoctor.com/2016-03-01/doctors?name=pete&location=37.773%2C-122.413%2C100&user_location=37.773%2C-122.413&skip=0&limit=10&user_key=5cbce404701e46ba0b6879e5692075eb`)
+    let jsonifiedResponse = await response.json();
+    console.log(response);
+    console.log(jsonifiedResponse);
+    return jsonifiedResponse;
   }
 }
--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
-import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
+import $ from 'jquery';
 import {Hospital} from './back.js';
 
 
@@ -11,25 +11,44 @@ $(document).ready(function(){
 
     const docName = $('#docName').val();
     $('#docName').val("");
-    const symptoms = $('#symptoms').val();
-    $('#symptoms').val("");
+    // const symptoms = $('#symptoms').val();
+    // $('#symptoms').val("");
+
+    // (async () => {
+    //   let hospital = new Hospital();
+    //   let response = await hospital.apiDocName(docName);
+    //   if (response.error) {
+    //     showError(response);
+    //   } else {
+    //     getElements(response);
+    //   }
+    // })();
+    //
+    // function getElements(response) {
+    //
+    // }
+    //
+    // function showError(response) {
+    //
+    // }
 
     (async () => {
       let hospital = new Hospital();
-      let response = await hospital.apiDocName(docName);
-      if (response.error) {
-        showError(response);
-      } else {
+      let response = await hospital.apiDocName(docName)
+      if (this.readyState === 4 && this.status === 200) {
         getElements(response);
+      } else {
+        showError(response)
       }
+
     })();
 
     function getElements(response) {
-
+      $('showNumber').text()
     }
 
     function showError(response) {
-
+      $('showNumber').text()
     }
 
   });
