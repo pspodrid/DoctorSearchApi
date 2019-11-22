9fd85eb3afbdde606fc0664b7820395bbe0c1e0e
Student <example@example.com>
2019-11-22 09:45:42 -0800

    add backend logic

--- src/main.js
+++ src/main.js
@@ -9,5 +9,28 @@ $(document).ready(function(){
   $('form').submit(function(event){
     event.preventDefault();
 
+    const docName = $('#docName').val();
+    $('#docName').val("");
+    const symptoms = $('#symptoms').val();
+    $('#symptoms').val("");
+
+    (async () => {
+      let hospital = new Hospital();
+      let response = await hospital.apiDocName(docName);
+      if (response.error) {
+        showError(response);
+      } else {
+        getElements(response);
+      }
+    })();
+
+    function getElements(response) {
+
+    }
+
+    function showError(response) {
+
+    }
+
   });
 });
