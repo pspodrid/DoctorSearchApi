e0162c98b7c130357fe6507cb27497d0bd78c6f3
Student <example@example.com>
2019-11-22 15:45:55 -0800

    try to get process.env.API_KEY to work

--- src/back.js
+++ src/back.js
@@ -2,7 +2,7 @@ export class Hospital {
 
   async apiDocName(docName) {
     try {
-      let response = await fetch(`https://api.betterdoctor.com/2016-03-01/doctors?name=${docName}&location=37.773%2C-122.413%2C100&user_location=37.773%2C-122.413&skip=0&limit=10&user_key=7b8655aeee50e564088f9e566b8e2611`);
+      let response = await fetch(`https://api.betterdoctor.com/2016-03-01/doctors?name=${docName}&location=37.773%2C-122.413%2C100&user_location=37.773%2C-122.413&skip=0&limit=10&user_key=${process.env.API_KEY}`);
       if(response.ok) {
         let jsonifiedResponse = await response.json();
         return jsonifiedResponse;
@@ -11,7 +11,7 @@ export class Hospital {
         throw new Error('my error');
       }
     } catch(error) {
-      let response = await fetch (`https://api.betterdoctor.com/2016-03-01/doctors?name=${docName}&location=37.773%2C-122.413%2C100&user_location=37.773%2C-122.413&skip=0&limit=10&user_key=7b8655aeee50e564088f9e566b8e2611`);
+      let response = await fetch (`https://api.betterdoctor.com/2016-03-01/doctors?name=${docName}&location=37.773%2C-122.413%2C100&user_location=37.773%2C-122.413&skip=0&limit=10&user_key=${process.env.API_KEY}`);
       if (!response.ok) {
         let jsonifiedResponse = await response.json();
         return jsonifiedResponse;
